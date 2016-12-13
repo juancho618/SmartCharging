3cc7c5e67eecd46a62dbdfa75b2d71893ffa3036
juancho618 <jjsorianoe@gmail.com>
2016-12-13 14:43:31 +0100

    Changes in station model

--- SmartCharging/ChargingAppointment/models.py
+++ SmartCharging/ChargingAppointment/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class ChargingSpot(models.Model):
     createdDate = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=255)
+    rate = models.IntegerField()
+    isFast = models.BooleanField()
+    plugType = models.CharField()
+    hasOwner = models.BooleanField()
+
