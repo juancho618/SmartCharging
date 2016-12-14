b263eb2f3ea2f789a46ce288bceb62a982cb455f
juancho618 <jjsorianoe@gmail.com>
2016-12-15 00:53:39 +0100

    Adding reset password

--- SmartCharging/SmartCharging/settings.py
+++ SmartCharging/SmartCharging/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_REDIRECT_URL = "listPlugType"
+
+#simulate email for reset password
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR,"sent_emails")
