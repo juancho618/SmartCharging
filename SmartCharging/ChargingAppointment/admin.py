eba14349d2df6c901b9e41f00b3eb4ce060c1980
juancho618 <jjsorianoe@gmail.com>
2016-12-21 14:58:43 +0100

    Appointment admin module

--- SmartCharging/ChargingAppointment/admin.py
+++ SmartCharging/ChargingAppointment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import  ChargingSpot, PlugType
+from .models import  ChargingSpot, PlugType, Appointment
 
 admin.site.register(ChargingSpot)
 admin.site.register(PlugType)
+admin.site.register(Appointment)
 # Register your models here.
